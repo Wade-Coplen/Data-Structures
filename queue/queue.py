96c20971bb40b727fa4a40cbfa5a90bfbf3fe9c7
Wade-Coplen <wadecoplen@gmail.com>
2020-09-16 21:33:27 -0700

    All tests passed for array in Q and Stacks

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,46 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = []
+        self.front = 0
     def __len__(self):
-        pass
+        return self.size
+    def is_empty(self):
+        return self.size == 0
 
     def enqueue(self, value):
-        pass
+        self.storage.append(value)
+        self.size = self.size + 1
+   
 
     def dequeue(self):
-        pass
+        if self.is_empty():
+            return None
+        value = self.storage[self.front]
+        self.storage[self.front] = None
+        self.front = self.front +1
+        self.size = self.size -1
+        return value
+    def first(self):
+        if self.is_empty():
+            return None
+        return self.storage[self.front]
+
+q = Queue()
+q.enqueue(5)
+q.enqueue(10)
+print('Q', q.storage)
+print('L', len(q))
+print('DQ', q.dequeue())
+print('Q', q.storage)
+q.enqueue(15)
+q.enqueue(20)
+print('Q', q.storage)
+print('First El', q.first())
+print('DQ', q.dequeue())
+print('Q', q.storage)
+
