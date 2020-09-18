df38ee94a30963fa44f01ddccb161ccfa106383d
Wade-Coplen <wadecoplen@gmail.com>
2020-09-17 18:17:12 -0700

    errors with list queues that I havent solved yet

--- queue/queue.py
+++ queue/queue.py
@@ -13,6 +13,7 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+"""
 
 class Queue:
     def __init__(self):
@@ -55,4 +56,147 @@ print('Q', q.storage)
 print('First El', q.first())
 print('DQ', q.dequeue())
 print('Q', q.storage)
+
+"""
+class Node:
+    __slots__ = 'element', 'next'
+    def __init__(self, value, element, next_node= None):
+        self.value = value
+        self.next_node = next_node
+        self.element = element
+    def get_value(self):
+        return self.value
+
+    def get_next_node(self):
+        return self.next_node
+
+    def set_next_node(self, new_next):
+        self.next_node = new_next
+    
+    
+    
+class LinkedList:
+    def __init__(self):
+            # what attributes do we need?
+            #set initial state of node
+            self.head = None
+            self.tail = None
+    def add_to_head(self, value):
+        # create a new Node
+        new_node = Node(value)
+        if self.head is None:
+            return None
+        else:
+            # set next_node of my newNode to the head
+            new_node.set_next_node(self.head)
+            # update head attribute            
+            self.head = new_node
+        
+    def add_to_tail(self, value):
+        # create a new Node
+        new_node = Node(value)
+        # if LL is empty
+        if self.head == None:
+            return None
+            #update head & tail attributes
+            self.head = new_node
+            self.tail = new_node
+        # 2. If LL is not empty
+        else:
+            # update next_node of our tail
+            self.tail.set_next_node(new_node)
+            #update self.tail
+            self.tail = new_node
+
+    def remove_head(self):
+        # if LL is empty
+        if self.head is None:
+            return None
+        else:
+            # return the value of the old head
+            return_value = self.head.get.value()
+            #list with 1 element
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            #lists with >2 elements
+            else:
+                self.head = self.head.get_next_node()
+                return return_value
+    def remove_tail(self):
+        # TODO
+        # if the there is an empty list?
+        if self.tail == None:
+            return None
+        else:
+            #value of previous head
+            # return_value = self.head.get.value()
+        # list with 1 element?
+            if self.tail == self.head:
+                self.head = None
+                self.tail = None
+            else:
+            # list with >2 elements?
+                self.head = self.head.get_next_node()
+                return return_value
+                
+        
+    def contains(self, value):
+        # TODO time permitting
+        current_node = self.head
+        while current_node == None:
+            # if we find 'value'
+            if current_node.get_value() == value:
+                return True
+            return False
+    def get_max(self):
+        # TODO time permitting
+        pass
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # self.storage = ?
+        self.head = None
+        self.tail = None
+    
+    def __len__(self):
+        return self.size
+    def is_empty(self):
+        return self.size == 0
+
+    def enqueue(self, element):
+        new_node = Node(element, None)
+        if self.is_empty():
+            self.head = new_node
+        else:
+            self.tail.nex t = new_node
+        self.tail = new_node
+        self.size = self.size + 1
+
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        value = self.head.elements
+        self.head = self.head.next_node
+        self.size = self.size -1
+        if self.is_empty():
+            self.tail = None
+        return value
+    def display(self):
+        temp = self.head
+        while temp:
+            temp = temp.next_node
+            print()
+
+q = Queue()
+q.enqueue(400)
+q.enqueue(401)
+q.display()
+print('L', len(q))
+print('DQ', q.dequeue())
+q.display()
+q.enqueue(402)
+q.enqueue(403)
+q.display()
 
--- stack/stack.py
+++ stack/stack.py
@@ -10,6 +10,142 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+
+class Node:
+    def __init__(self, value, next_node= None):
+        self.value = value
+        self.next_node = next_node
+    def get_value(self):
+        return self.value
+
+    def get_next_node(self):
+        return self.next_node
+
+    def set_next_node(self, new_next):
+        self.next_node = new_next
+    
+
+class LinkedList:
+    def __init__(self):
+            # what attributes do we need?
+            #set initial state of node
+            self.head = None
+            self.tail = None
+    def add_to_head(self, value):
+        # create a new Node
+        new_node = Node(value)
+        if self.head is None:
+            return None
+        else:
+            # set next_node of my newNode to the head
+            new_node.set_next_node(self.head)
+            # update head attribute            
+            self.head = new_node
+        
+    def add_to_tail(self, value):
+        # create a new Node
+        new_node = Node(value)
+        # if LL is empty
+        if self.head == None:
+            return None
+            #update head & tail attributes
+            self.head = new_node
+            self.tail = new_node
+        # 2. If LL is not empty
+        else:
+            # update next_node of our tail
+            self.tail.set_next_node(new_node)
+            #update self.tail
+            self.tail = new_node
+
+    def remove_head(self):
+        # if LL is empty
+        if self.head is None:
+            return None
+        else:
+            # return the value of the old head
+            return_value = self.head.get.value()
+            #list with 1 element
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            #lists with >2 elements
+            else:
+                self.head = self.head.get_next_node()
+                return return_value
+    def remove_tail(self):
+        # TODO
+        # if the there is an empty list?
+        if self.tail == None:
+            return None
+        else:
+            #value of previous head
+            # return_value = self.head.get.value()
+        # list with 1 element?
+            if self.tail == self.head:
+                self.head = None
+                self.tail = None
+            else:
+            # list with >2 elements?
+                self.head = self.head.get_next_node()
+                return return_value
+                
+        
+    def contains(self, value):
+        # TODO time permitting
+        current_node = self.head
+        while current_node == None:
+            # if we find 'value'
+            if current_node.get_value() == value:
+                return True
+            return False
+    def get_max(self):
+        # TODO time permitting
+        pass
+
+
+
+
+"""
+class Empty(Exception):
+    pass
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = []
+
+    def is_empty(self):
+        return len(self.storage) == 0
+
+    def __len__(self):
+        return len(self.storage)  
+        
+    def push(self, value):
+        """
+        Push the elements at the Last index
+        :return: None
+        """
+        self.storage.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.storage.pop()
+        
+s = Stack()
+s.push(10)
+s.push(20)
+s.push(30)
+s.push(40)
+print('Stack', s.storage)
+print('Length', len(s))
+print('Is empty', s.is_empty())
+print('Popped', s.pop())
+print('Stack', s.storage)
+"""
+
 class Stack:
     def __init__(self):
         self.size = 0
