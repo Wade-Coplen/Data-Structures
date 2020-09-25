7433eddf67fb1c80c0ff91dc7cc9839ba0fde2f2
Wade-Coplen <wadecoplen@gmail.com>
2020-09-24 18:09:36 -0700

    working on BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -14,53 +14,104 @@ class BSTNode:
         self.value = value
         self.left = None
         self.right = None
+    def __init__(self):
+        self.root = None
+        self.size = 0
 
     # Insert the given value into the tree
     def insert(self, value):
+        temp_root = self.root
+        temp_troot = None
+        while troot:
+            temp_troot = temp_root
+            if value < temp_root.value:
+                temp_root = temp_root.left
+            elif value > temp_root.value:
+                temp_root = temp_root.right
+            node = self.BSTNode(value)
+            if self.root:
+                if value < temp_troot.value:
+                    temp_troot.left = node
+                else:
+                    temp_troot.right = node
         #Recursive
             #if value < root
-        # if value is < root 
+        #if value < root:
+
             # if left child is None
                 # add here...something = something else
             #else:
-                self.left.insert(value) #recursive
+               # self.left.insert(value) #recursive
                 # exit loop
                 
         # IF value >= root, go right(dupes go tot the right)
             #if right child is None
                 # add here
             #else: 
-                self.right.insert(value) #recursive
+                #self.right.insert(value) #recursive
                 # exit loop
         pass
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
+        temp_root = self.root
+        while temp_root:
+            if target < temp_root.value:
+                temp_root = temp_root.left
+            elif target > troot.value:
+                temp_root = temp_root.right
+            else:
+                return True
+        return False
         # check to see if self.value is target
         # if yes return true
         # if no, go left or right?
 
-        pass
-
     # Return the maximum value found in the tree
-    def get_max(self):
+    def get_maxValue(self):
+        if self.root:
+            return self.get_max(self.root)
         # go right until you cannot go anymore
         # return the value at the far right
-        pass
+    def get_max(self, node):
+        if node.rightChild:
+            return self.get_max(node.rightChild)
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        # one side then the other
-        # fn(value)
-        pass
+        fn(self.value)
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
+
+        # go left, call fn() for each node
+        # go right, call fn() for each node
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
+
+    # what is our stopping point?
     def in_order_print(self):
-        pass
+        #using in_order, print nodes in non decreasing order
+        #4,2,5,1,3
+        #inorder(node):
+        #   if node == null then return
+        #   inorder(node.left)
+        #   visit(node)
+        #   inorder(node.right)
+        while self.root:
+            if self.left is None:
+                return self.root
+            if self.value < self.root:
+                return self.left
+            if self.value > self.root:
+                return self.right
+
+                    
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
